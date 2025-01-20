79ae9e5738cf4bf4884bc349c12c37b0dabbedd3
hym1222 <hayamiriona1222@gmail.com>
2025-01-20 18:44:17 +0900

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,32 @@
+Rails.application.routes.draw do
+  devise_for :users
+  resources :users, only:[:index, :show, :edit, :update]
+  resources :posts do
+    resources :comments, only:[:create, :destroy]
+    resource :favorites, only:[:create, :destroy]
+    collection do
+      get 'confirm'
+    end
+  end
+
+  resources :users, only:[:index, :show, :edit, :update] do
+    member do
+      get :follows, :followers
+    end
+    resource :relationships, only: [:create, :destroy]
+  end
+  
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "up" => "rails/health#show", as: :rails_health_check
+
+  # Render dynamic PWA files from app/views/pwa/*
+  get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+  get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
+
+  # Defines the root path route ("/")
+  # root "posts#index"
+  root :to => 'homes#top'
+end
