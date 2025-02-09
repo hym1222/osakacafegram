23e6f5f810592adb0b2a1310b9f62dfdd52e5a27
hym1222 <hayamiriona1222@gmail.com>
2025-02-09 18:54:46 +0900

    修正

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,9 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+# 必要に応じて以下を追加
+require 'sprockets/railtie'
+
 module Osakacafegram
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
